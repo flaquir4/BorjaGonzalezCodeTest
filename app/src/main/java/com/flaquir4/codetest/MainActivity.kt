cb6d01e299f16fbf2230577482cf5710995f4235
Borja González <flaquir4@gmail.com>
2020-07-20 18:45:34 +0200

    Add basic skeleton

---
+++ app/src/main/java/com/flaquir4/codetest/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.flaquir4.codetest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
