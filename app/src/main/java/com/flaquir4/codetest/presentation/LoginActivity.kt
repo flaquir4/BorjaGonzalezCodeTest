e423e6f3a21816415f0ec4fbf7c84e66741f8b50
Borja González <flaquir4@gmail.com>
2020-07-22 21:53:07 +0200

    Add main activity

--- app/src/main/java/com/flaquir4/codetest/presentation/LoginActivity.kt
+++ app/src/main/java/com/flaquir4/codetest/presentation/LoginActivity.kt
@@ -3,10 +3,15 @@ package com.flaquir4.codetest.presentation
 import android.os.Bundle
 import com.flaquir4.codetest.R
 import com.flaquir4.codetest.presentation.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        loginButton?.setOnClickListener {
+           startActivity(MainActivity.getIntent(this))
+        }
     }
 }
