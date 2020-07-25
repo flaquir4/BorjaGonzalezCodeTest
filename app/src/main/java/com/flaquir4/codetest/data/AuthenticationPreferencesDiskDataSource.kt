7aa8fc3ff99bc3aa770a649f16d72f1e5be1b0d4
borja <borja@helm.cat>
2020-07-25 11:51:50 +0200

    Make shared preferences operations in background

--- app/src/main/java/com/flaquir4/codetest/data/AuthenticationPreferencesDiskDataSource.kt
+++ app/src/main/java/com/flaquir4/codetest/data/AuthenticationPreferencesDiskDataSource.kt
@@ -5,6 +5,8 @@ import android.content.Context
 import androidx.core.content.edit
 import cat.helm.result.Result
 import com.flaquir4.codetest.domain.errors.AuthenticationError
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class AuthenticationPreferencesDiskDataSource @Inject constructor(private val context: Application) :
@@ -12,19 +14,25 @@ class AuthenticationPreferencesDiskDataSource @Inject constructor(private val co
 
     private val preferences = context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE)
 
-    override suspend fun saveToken(token: String): Result<Unit, AuthenticationError> = Result.of {
-        preferences.edit(commit = true) {
-            putString(TOKEN_KEY, token)
+    override suspend fun saveToken(token: String): Result<Unit, AuthenticationError> =
+        withContext(Dispatchers.IO) {
+            Result.of {
+                preferences.edit(commit = true) {
+                    putString(TOKEN_KEY, token)
+                }
+            }.mapError {
+                AuthenticationError.BadCredentials
+            }
         }
-    }.mapError {
-        AuthenticationError.BadCredentials
-    }
 
-    override suspend fun getToken(): Result<String, AuthenticationError> = Result.of {
-        preferences.getString(TOKEN_KEY, "")
-    }.mapError {
-        AuthenticationError.DiskError
-    }
+    override suspend fun getToken(): Result<String, AuthenticationError> =
+        withContext(Dispatchers.IO) {
+            Result.of {
+                preferences.getString(TOKEN_KEY, "")
+            }.mapError {
+                AuthenticationError.DiskError
+            }
+        }
 
     companion object {
         const val PREFERENCES_NAME = "CodeTest"
